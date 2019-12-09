a3656612e0b9ab7787ce744d6666a8ffe0ebf83f
Aaron Marks <aaron.marks@cdpehs.com>
2019-12-09 16:58:52 -0500

    Adding Angular version to home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public angularVersion: string;
+  constructor() {
+    this.angularVersion = VERSION.full;
+   }
 
   ngOnInit() {
   }
