6192129a8ccd117899d2f64fee8acc405536acfc
Aaron Marks <am1983@users.noreply.github.com>
2020-02-01 15:38:54 -0500

    Callout badges converted to component (#4)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { CardGalleryComponent } from './card-gallery/card-gallery.component';
+import { CalloutComponent } from './callout/callout.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    CardGalleryComponent
+    CardGalleryComponent,
+    CalloutComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    CardGalleryComponent
+    CardGalleryComponent,
+    CalloutComponent
   ]
 })
 export class SharedModule { }
