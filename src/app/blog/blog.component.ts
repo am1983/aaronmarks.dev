f3275e5b8783a2f2625d0b79013b52624a7842bf
Aaron Marks <am1983@users.noreply.github.com>
2020-02-01 19:24:48 -0500

    Prismic integration (#5)
    * Adds Prismic API and blog component
    
    * Cleaning up HTML serializer
    
    * Adding info on Prismic to home page
    
    * Fixing deep linking issue.

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import PrismicDOM from 'prismic-dom';
+import { PrismicService } from '../prismic.service';
+
+@Component({
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.scss']
+})
+export class BlogComponent implements OnInit {
+  // tslint:disable-next-line: ban-types
+  posts: void | Object[];
+  isBusy = false;
+  PrismicDOM = PrismicDOM;
+
+  constructor(private prismicService: PrismicService) { }
+
+  ngOnInit() {
+    this.getBlog();
+  }
+
+  getBlog(): void {
+    this.isBusy = true;
+
+    this.prismicService.test().then(posts => {
+      this.posts = posts;
+      this.isBusy = false;
+    });
+    console.dir(this.posts);
+  }
+
+  htmlSerializer(type, element, content, children) {
+    const Elements = PrismicDOM.RichText.Elements;
+    switch (type) {
+      // Add a class to paragraph elements
+      case Elements.paragraph:
+        if (children[0].length > 0) {
+          // Paragraph isn't empty... so it isn't a line break.
+          return '<p class="py-4">' + children.join('') + '</p>';
+        }
+        return null;
+      // Don't wrap images in a <p> tag
+      default:
+        return null;
+    }
+  }
+}
---
+++ src/prismic-configuration.ts
@@ -0,0 +1,13 @@
+export interface PrismicConfiguration {
+    apiEndpoint: string;
+    accessToken?: string;
+    // tslint:disable-next-line: ban-types
+    linkResolver: Function;
+}
+
+export const CONFIG: PrismicConfiguration = {
+    apiEndpoint: 'https://blog-aaronmarks-dev.cdn.prismic.io/api/v2',
+    linkResolver(doc) {
+        return '/';
+    }
+};
